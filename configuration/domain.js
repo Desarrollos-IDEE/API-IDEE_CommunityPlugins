19ed69dce79c1fde5e71289f955db43057c1fd6b
Carmen <carmenmvg@gmail.com>
2025-11-25 17:27:56 +0100

    cambio de ruta

--- configuration/domain.js
+++ configuration/domain.js
@@ -1,2 +1,2 @@
-const IDEE_DOMAIN = 'https://componentes-desarrollo.idee.es/api-idee';
+const IDEE_DOMAIN = 'https://componentes.idee.es/api-idee';
 const IDEE_STATIC_RESOURCES = 'https://componentes.idee.es/estaticos';
